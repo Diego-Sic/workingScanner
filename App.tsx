2a06ab6b80c300636f58a48d15938a4594e855c8
Diego Sic <diego.sic@centre.edu>
2024-04-14 19:08:34 -0400

    Working version of codeScanner

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { SafeAreaView } from "react-native";
-import CodeScanner from "./Scanner";
+import CodeScanner from "./scanner";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <CodeScanner></CodeScanner>
+      <CodeScanner />
     </SafeAreaView>
   );
 }
